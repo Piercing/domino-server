74b11e6b6946df10a803b42a23a1648d5c2377f8
juliocesar.matabielmas@gmail.com <juliocesar.matabielmas@gmail.com@0aca1984-d2ad-dfa0-ddb1-866a851f5b05>
2012-04-25 06:11:11 +0000

    Cambios para el tren modificado

--- trunk/domino-server/src/main/java/itesm/distrib/Tren.java
+++ trunk/domino-server/src/main/java/itesm/distrib/Tren.java
@@ -6,14 +6,22 @@ class Tren {
 
     private int numero;
     private ArrayList<Ficha> fichas;
+    private boolean marcado;
+    private boolean principal;
 
-    public int getNumero() {
-        return numero;
+    public int getNumero() 
+    { 
+        if(!principal){ return numero; }
+        else{ return -1; }        
     }
-
-    public void setNumero(int value) {
-        numero = value;
-    }
+    
+    public void setNumero(int value) { numero = value; }
+    
+    public boolean getMarcado(){ return marcado;}
+    public void setMarcado(boolean value) { marcado = value; }
+    
+    public boolean esPrincipal() { return principal;}
+    public void setEsPrincipal(boolean value) { principal = value; }
 
     public void agregarFicha(Ficha ficha) {
         fichas.add(ficha);
@@ -43,22 +51,27 @@ class Tren {
     public Tren(String str) {
         this();
         String[] a = str.split(";");
-        if (a.length >= 1) {
-            setNumero(Integer.parseInt(a[0]));
-            if (a.length == 2) {
-                String[] strFichas = a[1].split(",");
-                for (int i = 0; i < strFichas.length; i++) {
-                    Ficha f = new Ficha(strFichas[i]);
-                    this.agregarFicha(f);
-                }
+        if (a.length == 4) {
+            setEsPrincipal(Boolean.parseBoolean(a[0]));            
+            setNumero(Integer.parseInt(a[1]));
+            setMarcado(Boolean.parseBoolean(a[2]));
+            String[] strFichas = a[3].split(",");
+            for (int i = 0; i < strFichas.length; i++) {
+                Ficha f = new Ficha(strFichas[i]);
+                this.agregarFicha(f);
             }
         }
     }
+    
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        sb.append(String.valueOf(principal));
+        sb.append(";");
         sb.append(String.valueOf(numero));
         sb.append(";");
+        sb.append(String.valueOf(marcado));
+        sb.append(";");        
         String coma = "";
         Iterator<Ficha> i = fichas.iterator();
         while (i.hasNext()) {
